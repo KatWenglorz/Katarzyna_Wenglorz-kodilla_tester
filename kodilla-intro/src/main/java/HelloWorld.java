76be1035193873766e41a2d66eef720dbb8f7368
Katarzyna Wenglorz <kat.wenglorz@gmail.com>
2022-10-04 12:03:39 +0200

    1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Kasia");
+        System.out.println(33);
+        System.out.println('W');
+    }
+}
