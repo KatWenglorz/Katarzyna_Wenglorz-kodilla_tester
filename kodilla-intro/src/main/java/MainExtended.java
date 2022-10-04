10ed001311078e3d0bf3311a511d7dbcfb2409d8
Katarzyna Wenglorz <kat.wenglorz@gmail.com>
2022-10-04 14:41:09 +0200

    2

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String country = "Polska";
+        String month = "październik";
+        int year = 2022;
+        int dayOfTheMonth = 4;
+        double pi = 3.14;
+        char houseNumber = 'B';
+        boolean isTheSkyBlue = true;
+        System.out.println(country);
+        System.out.println(month);
+        System.out.println(year);
+        System.out.println(dayOfTheMonth);
+        System.out.println(pi);
+        System.out.println(houseNumber);
+        System.out.println(isTheSkyBlue);
+    }
+}
