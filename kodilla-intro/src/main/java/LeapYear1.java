10f7f9140a3133d67628a5558ee996e0ce38476d
Katarzyna Wenglorz <kat.wenglorz@gmail.com>
2022-10-17 20:33:06 +0200

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear1.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.SynchronousQueue;
+
+public class LeapYear1 {
+    public static void main(String[] args) {
+
+        int year = 2022;
+
+        if ((year%400 == 0) || (year%4 == 0 && year%100 > 0)) {
+            System.out.println(year + " jest rokiem przestępnym.");
+        } else {
+            System.out.println(year + " nie jest rokiem przestępnym.");
+        }
+    }
+}
