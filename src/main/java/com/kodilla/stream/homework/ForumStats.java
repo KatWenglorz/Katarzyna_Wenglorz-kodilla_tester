ef4b37687fa2b336319f858d268c962c8accfc29
Katarzyna Wenglorz <kat.wenglorz@gmail.com>
2022-12-10 22:21:16 +0100

    1

---
+++ src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,33 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import com.kodilla.stream.UsersRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        System.out.println(averageNumberOfPostsOfUsersUnder40(UsersRepository.getUsersList()));
+        System.out.println(averageNumberOfPostsOfUsersOver40(UsersRepository.getUsersList()));
+    }
+    public static double averageNumberOfPostsOfUsersUnder40(List<User> usersList) {
+       double avg = usersList
+        .stream()
+                .filter(user -> user.getAge() < 40)
+                .mapToInt(n -> n.getNumberOfPost())
+               .average()
+               .getAsDouble();
+       return avg;
+    }
+    public static double averageNumberOfPostsOfUsersOver40(List<User> usersList) {
+        double avg1 = usersList
+                .stream()
+                .filter(user -> user.getAge() >= 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        return avg1;
+    }
+}
---
+++ src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -0,0 +1,52 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import com.kodilla.stream.UsersRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForumStatsTest {
+    @Test
+    public void testAverageNumberOfPostsOfUsersUnder40() {
+        //given
+        List<User> usersList = new ArrayList<>();
+        usersList.add(new User("Jessie Pinkman", 25, 4648, "Sales"));
+        usersList.add(new User("Walter White", 50, 7, "Chemists"));
+        //when
+        double result = ForumStats.averageNumberOfPostsOfUsersUnder40(usersList);
+        //then
+        double expected = 4648;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testAverageNumberOfPostsOfUsersOver40() {
+        //given
+        List<User> usersList = new ArrayList<>();
+        usersList.add(new User("Walter White", 50, 8, "Chemists"));
+        usersList.add(new User("Mike Ehrmantraut", 57, 0, "Security"));
+        usersList.add(new User("Tuco Salamanca", 34, 116, "Manager"));
+        //when
+        double result = ForumStats.averageNumberOfPostsOfUsersOver40(usersList);
+        //then
+        double expected = 4;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testAverageNumberOfPostsOfUsersOver40WithZeroUsers() {
+        //given
+        List<User> usersList = new ArrayList<>();
+        usersList.add(new User("Mike Ehrmantraut", 20, 0, "Security"));
+        usersList.add(new User("Tuco Salamanca", 34, 116, "Manager"));
+        //when
+        double result = ForumStats.averageNumberOfPostsOfUsersOver40(usersList);
+        //then
+        double expected = 0;
+        assertEquals(result, expected);
+    }
+}
